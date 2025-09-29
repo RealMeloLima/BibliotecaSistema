6caef29a0ed6f846688ac8f1d552406dfb63aaf2
Paulo Rogério <80416920+RealMeloLima@users.noreply.github.com>
2025-09-29 13:59:03 -0300

    Add files via upload

---
+++ BibliotecaSis/src/biblioteca/model/Aluno.java
@@ -0,0 +1,19 @@
+package biblioteca.model;
+
+
+public class Aluno extends Usuario {
+
+    public Aluno(String matricula, String nome, String curso) {
+        super(matricula, nome, curso);
+    }
+
+    @Override
+    public int getPrazoDias() {
+        return 7;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno: " + super.getNome() + " (Matrícula: " + super.getMatricula() + ")";
+    }
+}
